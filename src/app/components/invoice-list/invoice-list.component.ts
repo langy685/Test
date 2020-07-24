19e8951fcc575cb841ea3f5465cb15512119f42d
Marek Lakostik <lakostikms@kros.sk>
2020-07-24 12:10:56 +0200

    arr indexes

--- src/app/components/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice-list/invoice-list.component.ts
@@ -27,7 +27,7 @@ export class InvoiceListComponent implements OnInit, OnDestroy {
   }
 
   private initInvoices() {
-    for(let i=1;i<=500;i++) {
+    for (let i = 1; i <= 500; i++) {
       this.invoices.push(this.createInvoice(i, this.randomString()));
     }
     this.invoiceManagerService.invoicesStateChanged(this.getInvoicesState());
@@ -44,28 +44,35 @@ export class InvoiceListComponent implements OnInit, OnDestroy {
   private getInvoicesState(): InvoiceState {
     let paid = this.invoices.filter(inv => inv.price > 0.5 && inv.visible).length;
     let unpaid = this.invoices.filter(inv => inv.price <= 0.5 && inv.visible).length;
-    console.log('paid', paid);
-    console.log('unpaid', unpaid);
-    return { paid, unpaid};
+    return {paid, unpaid};
   }
 
   private subscribeToInvoiceAdded() {
     this.subs.sink = this.invoiceManagerService.invoiceAdded$.subscribe(
       invoiceName => {
-        if (invoiceName !== ''){
+        if (invoiceName !== '') {
           this.invoices.splice(0, 0, this.createInvoice(this.invoices.length + 1, invoiceName))
+          this.buildItemIndexes();
           this.invoiceManagerService.invoicesStateChanged(this.getInvoicesState());
         }
       }
     )
   }
 
+  private buildItemIndexes() {
+    let newInvoicesArr: Invoice[] = [];
+    for (let i = 0; i < this.invoices.length; i++) {
+      newInvoicesArr.push({...this.invoices[i], index: i+1});
+    }
+    this.invoices = newInvoicesArr;
+  }
+
   private invoiceVisibilityChanged() {
     this.invoiceManagerService.invoicesStateChanged(this.getInvoicesState());
   }
 
-  private createInvoice(index:number, name: string): Invoice {
-    return ({index, name, price:this.randomNumber(0,1), visible:true}) as Invoice;
+  private createInvoice(index: number, name: string): Invoice {
+    return ({index, name, price: this.randomNumber(0, 1), visible: true}) as Invoice;
   }
 
 }
