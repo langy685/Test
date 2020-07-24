3d0ae057c9f5736da5dc4c74080df267fcfcc0ac
Marek Lakostik <lakostikms@kros.sk>
2020-07-24 11:32:56 +0200

    summary

--- src/app/components/invoice-list-summary/invoice-list-summary.component.ts
+++ src/app/components/invoice-list-summary/invoice-list-summary.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceListSummaryComponent implements OnInit {
 
+  paidCount:number;
+  unpaidCount:number;
+
   constructor() { }
 
   ngOnInit() {
