1ed34b4697df9c34b5801dbf919e98a01d4b1750
Marek Lakostik <lakostikms@kros.sk>
2020-07-24 10:57:07 +0200

    search works

--- src/app/components/invoice-list-search/invoice-list-search.component.ts
+++ src/app/components/invoice-list-search/invoice-list-search.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {InvoiceManagerService} from "../../services/invoice-manager.service";
+import { SubSink} from "subsink";
+import {debounceTime, distinctUntilChanged} from "rxjs/operators";
 
 @Component({
-  selector: 'app-invoice-list-header',
+  selector: 'app-invoice-list-search',
   templateUrl: './invoice-list-search.component.html',
   styleUrls: ['./invoice-list-search.component.scss']
 })
-export class InvoiceListSearchComponent implements OnInit {
+export class InvoiceListSearchComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  searchForm: FormGroup;
+  subs = new SubSink();
+
+  constructor(
+    private invoiceManagerService: InvoiceManagerService,
+    private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.initForm();
+    this.subscribeToSearch();
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
+  private initForm() {
+    this.searchForm = this.formBuilder.group({
+      search: '',
+    })
+  }
+
+  private subscribeToSearch() {
+    this.subs.sink = this.searchForm.controls['search'].valueChanges.pipe(
+      distinctUntilChanged(),
+      debounceTime(300),
+    ).subscribe(
+      text => {
+        this.invoiceManagerService.invoiceSearch(text);
+      }
+    )
   }
 
 }
--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -1,18 +1,34 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Invoice} from "../../models/invoice";
+import {SubSink} from "subsink";
+import {InvoiceManagerService} from "../../services/invoice-manager.service";
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss']
 })
-export class InvoiceComponent implements OnInit {
+export class InvoiceComponent implements OnInit, OnDestroy {
 
   @Input() invoice:Invoice;
 
-  constructor() { }
+  subs = new SubSink();
+  found: boolean;
+
+  constructor(private invoiceManagerService: InvoiceManagerService) { }
 
   ngOnInit() {
+    this.subscribeToInvoiceSearch();
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
+  private subscribeToInvoiceSearch() {
+    this.subs.sink = this.invoiceManagerService.invoiceSearch$.subscribe(
+      invoiceName => this.found = this.invoice.name.includes(invoiceName)
+    )
   }
 
 }
